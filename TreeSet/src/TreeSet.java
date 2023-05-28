7da6b2e7935c3fb0b9aca179b606f82f6c4a6d93
Nikita Kazancev <128709443+JustGister@users.noreply.github.com>
2023-05-28 20:48:34 +0700

    Update TreeSet.java

--- TreeSet/src/TreeSet.java
+++ TreeSet/src/TreeSet.java
@@ -1,21 +1,165 @@
-import java.util.*;
+public class TreeSet<k, v> {
 
-public class TreeSet <E extends Comparable<E>>{
-    private TreeMap<E, Object> map;
+    private int size = 0;
+    private Node root = null;
 
-    public TreeSet() {
-        map = new TreeMap<>();
+    private class Node{
+        public k key;
+        public v value;
+        public Node left,right;
+
+        public Node(k key,v value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+        }
     }
 
-    public void add(E element) {
-        map.put(element, null);
+    private Node findNode(Object key) {
+        Node currentNode = root;
+        Comparable<k> cKey = (Comparable<k>) key;
+        while(currentNode != null) {
+            int num = cKey.compareTo(currentNode.key);
+            if(num < 0) {
+                currentNode = currentNode.left;
+            }
+            if(num > 0) {
+                currentNode = currentNode.right;
+            }
+            if(num == 0){
+                return currentNode;
+            }
+        }
+        return null;
     }
 
-    public void remove(E element) {
-        map.remove(element);
+    public v contains(Object key) {
+        Node thisNode = findNode(key);
+        if(thisNode == null) {
+            return null;
+        }
+        return thisNode.value;
     }
 
-    public boolean contains(E element) {
-        return map.containsKey(element);
+    private v addHelper(k key, v value, Node node) {
+        Comparable<k> cKey = (Comparable<k>) key;
+        int num = cKey.compareTo(node.key);
+        if(num < 0) {
+            if(node.left == null) {
+                node.left = new Node(key, value);
+                size++;
+                return null;
+            }
+            return addHelper(key, value, node.left);
+        }
+        if(num > 0) {
+            if(node.right == null) {
+                node.right = new Node(key, value);
+                size++;
+                return null;
+            }
+            return addHelper(key, value, node.right);
+        }
+        if(num == 0){
+            v old = node.value;
+            node.value = value;
+            return old;
+        }
+        return null;
+    }
+
+    public v add(k key,v val) {
+        if(root == null) {
+            root = new Node(key, val);
+            size++;
+            return null;
+        }
+        return addHelper(key, val, root);
+    }
+
+    public v remove(Object key) {
+        Node child = findNode(key);
+        if(child == null) {
+            return null;
+        }
+        if(size==1) {
+            root=null;
+            size--;
+            return child.value;
+        }
+        Node parent = findParent(key);
+        if(child.left != null && child.right != null) {
+            Node find = find(child.right);
+            Node helpFind = findParent(find.key);
+            removeHelper(find, helpFind);
+            child.key = find.key;
+            v oldVal = child.value;
+            child.value = find.value;
+            return oldVal;
+        }
+        else {
+            return removeHelper(child, parent);
+        }
+    }
+    private Node findParent(Object key) {
+        Comparable<k> cKey = (Comparable<k>) key;
+        Node parent = root;
+        Node children = root;
+        while(children != null) {
+            int num = cKey.compareTo(children.key);
+            if(num < 0) {
+                parent = children;
+                children = children.left;
+            }
+            if(num > 0) {
+                parent = children;
+                children = children.right;
+            }
+            if(num == 0) {
+                return parent;
+            }
+        }
+        return null;
+    }
+    private v removeHelper(Node child, Node parent) {
+        if(child.left == null && child.right == null) {
+            if(parent.left == child) {
+                parent.left = null;
+            }
+            if(parent.right == child) {
+                parent.right = null;
+            }
+            size--;
+            return child.value;
+        }
+        if(parent.left == null) {
+            if(parent.left == child) {
+                parent.left = child.right;
+            }
+            if(parent.right == child) {
+                parent.right = child.right;
+            }
+            size--;
+            return child.value;
+        }
+        if(child.right == null) {
+            if(parent.left == child) {
+                parent.left = child.left;
+            }
+            if(parent.right == child) {
+                parent.right = child.left;
+            }
+            size--;
+            return child.value;
+        }
+        return null;
+    }
+
+    private Node find(Node node) {
+        while(node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 }
